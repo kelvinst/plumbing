3057a49802d323bab902a688ff9a8a31c136b616
kelvinst <kelvin.stinghen@gmail.com>
2018-01-19 13:44:33 -0200

    Adding pipe and tee

--- lib/plumbing.ex
+++ lib/plumbing.ex
@@ -1,18 +1,43 @@
 defmodule Plumbing do
   @moduledoc """
-  Documentation for Plumbing.
+  Plumbing has some useful function to work with elixir pipes.
+
+  Check the list of functions below.
   """
 
   @doc """
-  Hello world.
+  Pipes the `subject` through the given anonymous `function` and
+  returns `function`'s result.
+
+  This function is useful to pipe the subject through an anonymous
+  function, without having to do `1 |> (&to_string/1).()`, which is
+  a little bit to much parens.
 
   ## Examples
 
-      iex> Plumbing.hello
-      :world
+      iex> 1 |> Plumbing.pipe(&to_string/1)
+      "1"
 
   """
-  def hello do
-    :world
+  def pipe(subject, function), do: function.(subject)
+
+  @doc """
+  Pipes the `subject` through the given anonymous `function` but
+  always returns `subject`.
+
+  This function might be useful in situations you want to transform
+  the subject in something else and print it, or to save it to
+  a file. Basically anything which may generate some side effects but
+  you don't care about the results.
+
+  ## Examples
+
+      iex> 1 |> Plumbing.tee(&to_string/1) |> Kernel.+(1)
+      2
+
+  """
+  def tee(subject, function) do
+    function.(subject)
+    subject
   end
 end
--- test/plumbing_test.exs
+++ test/plumbing_test.exs
@@ -2,7 +2,11 @@ defmodule PlumbingTest do
   use ExUnit.Case
   doctest Plumbing
 
-  test "greets the world" do
-    assert Plumbing.hello() == :world
+  import ExUnit.CaptureIO
+
+  test "tee/2 always returns the subject" do
+    assert capture_io(fn ->
+      assert Plumbing.tee("This is a tee!", &IO.inspect(String.length(&1))) == "This is a tee!"
+    end) == "14\n"
   end
 end
